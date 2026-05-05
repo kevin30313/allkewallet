f94161c2eac53d0d35208a80dcddab69f7e13776
kevin30313 <131216280+kevin30313@users.noreply.github.com>
2026-05-05 15:26:00 -0400

    Fix: Configuración de CORS y rutas para producción

--- alkewallet-backend/src/main/java/com/kevin30313/alkewallet/config/SecurityConfig.java
+++ alkewallet-backend/src/main/java/com/kevin30313/alkewallet/config/SecurityConfig.java
@@ -30,7 +30,9 @@ public class SecurityConfig {
                 CorsConfiguration config = new CorsConfiguration();
                 config.setAllowedOrigins(List.of(
                     "https://alkewallet-frontend.onrender.com",
-                    "http://localhost:5173" // Para que sigas probando local si quieres
+                    "https://alkewallet-frontend.onrender.com/",
+                    "http://localhost:5173",
+                     "http://127.0.0.1:5173"
                 ));
                 config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                 config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
