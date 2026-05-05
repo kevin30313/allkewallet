9e85dd1d77e60d7bb3f5a24b31563dba6638be25
kevin30313 <131216280+kevin30313@users.noreply.github.com>
2026-05-05 16:24:17 -0400

    Fix: corregida URL de la API (eliminada L extra)

--- alkewallet-frontend/src/api.js
+++ alkewallet-frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     
-    baseURL: 'https://allkewallet-api.onrender.com',
+    baseURL: 'https://alkewallet-api.onrender.com',
 });
 
 // Este "Interceptor" es como un guardia de seguridad: 
